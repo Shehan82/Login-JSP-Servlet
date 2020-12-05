473e2d55f462e719656bcb40046b477860629d05
shehan82 <shehansandeepa82@gmail.com>
2020-12-05 22:36:29 +0530

    add function for getting userType

---
+++ src/main/java/LoginDAO.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class LoginDAO {
+    public int userType(UserCredentialsModel user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/school", "root","");
+
+        String query = "select userType from users where userName=? and password=?";
+        PreparedStatement pst = con.prepareStatement(query);
+        pst.setString(1, user.getUserName());
+        pst.setString(2, user.getPassword());
+        ResultSet rs = pst.executeQuery();
+        rs.next();
+        int userType = rs.getInt(1);
+
+        return userType;
+
+    }
+}
